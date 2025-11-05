60f738972fd34c82c21000b6e3ec0d84c6eec6d5
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-11-05 10:14:41 +0000

    Use new characteristics key and reference data
    This replaces the use of the deprecated property `characteristics`
    in both the Cas3Premises and Cas3Bedspace types.
    
    The reference data used for generating sets of characteristics
    radio buttons is now fetched from the CAS3-namespaced API endpoint
    if the ENABLE_CAS3V2_API flag is enabled.

--- server/paths/api.ts
+++ server/paths/api.ts
@@ -126,6 +126,7 @@ const cas3Api = {
     index: assessmentsCas3Path,
     notes: singleAssessmentCas3Path.path('referral-history-notes'),
   },
+  referenceData: cas3Path.path('reference-data'),
 }
 
 export default {
